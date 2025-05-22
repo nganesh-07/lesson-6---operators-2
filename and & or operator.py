bdf05db7468d194d13cd7ab90e12f014c588d33b
nganesh <nganesh@iu.edu>
2025-05-22 12:36:33 -0700

    and or operators

---
+++ and & or operator.py
@@ -0,0 +1,25 @@
+a = "spain"
+b = "italy"
+c = "greece"
+d = ""
+e = ""
+
+if a and b and c and d:
+    print("save ur money")
+
+else:
+    print("spend ur money!")
+
+# because the variable d doesn't have a value, it printed the else statement
+
+if b or c:
+    print("go shopping")
+
+else:
+    print("borrow clothes from neighbor")
+
+if d or e:
+    print("ask for travel advice")
+
+else:
+    print("create ur own itinerary")
