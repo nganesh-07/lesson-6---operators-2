d22cf0a9aafadafe3a64dac72f8b1b84d25a1236
nganesh <nganesh@iu.edu>
2025-05-22 12:45:40 -0700

    not equal to operator

---
+++ not equal to operatory.py
@@ -0,0 +1,18 @@
+a = 7
+b = 9
+c = 9
+
+print(a != b)
+print(b != c)
+
+a = "spain"
+b = "greece"
+
+if a != b:
+    print(a, "is not the same as", b)
+
+a = "mars"
+b = "the sun"
+
+if (a==7) == (b=="the sun"):
+    print("bye")
