7fb38339feccd2de74a2f0064f434647d381777d
nganesh <nganesh@iu.edu>
2025-05-29 19:12:03 -0700

    project 6

---
+++ project.py
@@ -0,0 +1,6 @@
+character = input("Enter a character here:")
+
+if (character >= "a" and character <= "z") or (character >= "A" and character <= "Z"):
+    print("This is an alphabet.")
+else: 
+    print("This is not an alphabet.")
